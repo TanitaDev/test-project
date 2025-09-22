f6bafa881fdf24e36f743b773c71ce6a7dfa204f
Tanita <tanita.pak@gmail.com>
2025-09-22 21:26:12 +0500

    delete age field

--- Animal.java
+++ Animal.java
@@ -1,17 +1,7 @@
 public abstract class Animal {
     private String name;
-    private int age;
 
-    public Animal(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
+    public Animal(String name) {
         this.name = name;
     }
 }
